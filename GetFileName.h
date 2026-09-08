da29109399293e8fe0ba20510964ceb03c041b91
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-08 04:03:54 -0400

    lab 2 files

---
+++ GetFileName.h
@@ -0,0 +1,19 @@
+#ifndef _GetFileName_H
+#define _GetFileName_H
+
+#include <iostream>
+#include <string>
+#include <stdbool.h>
+
+using namespace std;
+
+// Function to get the filename from command line arguments
+string GetFileName(int argc, char* argv[]);
+
+
+//Determines if number is perfect
+bool isHarshad(int number);
+
+#endif // _GetFileName_H	
+
+
