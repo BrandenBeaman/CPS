30a90974670dcfa7454f5253c86153b7c66fe3b8
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-08 23:12:18 -0400

    swapped lab2 main for correct code

--- Lab_2/Lab_2.cpp
+++ Lab_2/Lab_2.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "GetFileName.h"
+#include "harshad.h"
 
 using namespace std;
 
@@ -15,19 +15,23 @@ int main(int argc, char* argv[])
 
 	//declare a variable to hold the file handle
 	string line;
-	ifstream fileInput(baseName + ".in");
-	ofstream fileOutput(baseName + ".out");
+	ifstream fileInput;
+	ofstream fileOutput;
 
-	int fileNum;
+	int fileNum = 0;
 
 	//open the file given by the user to read the numbers from
 	fileInput.open(baseName + ".in");
+	fileOutput.open(baseName + ".out");
 
 	//read a line of text from the file and operate on that line
 	if (fileInput.is_open()) {
 
-	// while there is data in the file, store number from current line into fileNum, cant use getliine because it will read the line but then using >> to assign fileNum will not work because the line has already been read and itll skip to the next line
-		while (fileInput >> fileNum) {
+		// while there is data in the file, store number from current line into fileNum, cant use getliine because it will read the line but then using >> to assign fileNum will not work because the line has already been read and itll skip to the next line
+		while (getline(fileInput, line)) {
+
+			fileNum = stoi(line);
+
 			if (isHarshad(fileNum)) {
 				fileOutput << fileNum << " is a Harshad number" << endl;
 			}//if
@@ -35,11 +39,10 @@ int main(int argc, char* argv[])
 				fileOutput << fileNum << " is not a Harshad number" << endl;
 			}//else
 		}//while()
-
 		fileInput.close();
 		fileOutput.close();
+
 	} //if
 
 	return 0;
 } //main() 
-
