135103e0c0c7f4b0a6601d3f83baebcc844d340e
BrandenBeaman <brandenbeaman0@gmail.com>
2026-09-08 23:41:04 -0400

    fixed harshad.h comments

--- Lab_2/harshad.h
+++ Lab_2/harshad.h
@@ -8,10 +8,13 @@
 using namespace std;
 
 // Function to get the base file name from user in command line
+// User will be prompted to enter a valid file name until a valid file is found
+// User only should insert the base file name without the .in extension
+// The function will return the base file name without the .in extension
 string GetFileName(int argc, char* argv[]);
 
 
-//Determines if number is perfect
+//Determines if number from provided file is perfect or not
 bool isHarshad(int number);
 
 #endif // _harshad_H	
